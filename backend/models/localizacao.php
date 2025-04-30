06c9ce452f83690735bef584b0dc64b1567bd641
Francisco <30954@esjaloures.org>
2025-04-30 16:57:48 +0100

    acabar models

--- backend/models/localizacao.php
+++ backend/models/localizacao.php
@@ -1,1 +1,62 @@
-//francisco
+<?php
+// Francisco
+
+class Localizacao {
+    public $id;
+    public $idSala;
+    public $idParentLocation;
+    public $idEquipamento;
+    public $idLocalizacaoCategoria;
+    public $nome;
+    public $descricao;
+
+
+    public function __construct($id, $idSala, $idParentLocation, $idEquipamento, $idLocalizacaoCategoria, $nome, $descricao) {
+        $this->id = $id;
+        $this->idSala = $idSala;
+        $this->idParentLocation = $idParentLocation;
+        $this->idEquipamento = $idEquipamento;
+        $this->idLocalizacaoCategoria = $idLocalizacaoCategoria;
+        $this->nome = $nome;
+        $this->descricao = $descricao;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getIdSala() {
+        return $this->idSala;
+    }
+
+    public function getIdParentLocation() {
+        return $this->idParentLocation;
+    }
+
+    public function getIdEquipamento() {
+        return $this->idEquipamento;
+    }
+
+    public function getIdLocalizacaoCategoria() {
+        return $this->idLocalizacaoCategoria;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getDescricao() {
+        return $this->descricao;
+    }
+
+    public function toString(){
+        return ("Id: " . $this->id . 
+                ", IdSala: " . $this->idSala . 
+                ", IdParentLocation: " . $this->idParentLocation . 
+                ", IdEquipamento: " . $this->idEquipamento . 
+                ", IdLocalizacaoCategoria: " . $this->idLocalizacaoCategoria . 
+                ", Nome: " . $this->nome . 
+                ", Descricao: " . $this->descricao);
+    }
+}
+?>
--- backend/models/sala.php
+++ backend/models/sala.php
@@ -1,1 +1,33 @@
-//francisco
+<?php
+// Francisco
+
+class Sala {
+    public $id;
+    public $nome;
+    public $piso;
+
+    public function __construct($id, $nome, $piso) {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->piso = $piso;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getPiso() {
+        return $this->piso;
+    }
+
+    public function toString(){
+        return ("Id: " . $this->id . 
+                ", Nome: " . $this->nome . 
+                ", Piso: " . $this->piso);
+    }
+}
+?>
