3f82e4928afe94a61ce5bdf3a069a5be18ed2172
Gonçalo <gongasjesus08@gmail.com>
2025-04-30 16:35:37 +0100

    finalizacao dos models cargo, estado e equipamentocategoria.

--- backend/models/cargo.php
+++ backend/models/cargo.php
@@ -1,1 +1,25 @@
-gonçalo
+<?php
+//gonçalo
+
+class Cargo{
+    public $id;
+    public $nome;
+
+    public function __construct($id, $nome) {
+        $this->id = $id;
+        $this->nome = $nome;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function toString(){
+        return ('id: ' . $this->id . ', nome: ' . $this->nome );
+    }
+}
+?>
