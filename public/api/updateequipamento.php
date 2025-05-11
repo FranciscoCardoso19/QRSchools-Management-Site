cd4a8219b4f92013067c1c98581559917651d3ef
André <37020@esjaloures.org>
2025-05-12 00:55:16 +0100

    update equipamentos.

--- public/api/updateequipamento.php
+++ public/api/updateequipamento.php
@@ -2,7 +2,7 @@
 require '../../vendor/autoload.php';
 
 include_once('../../backend/db.php');
-include_once('../../backend/models/user.php');
+include_once('../../backend/models/equipamento.php');
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
@@ -27,24 +27,28 @@ try {
 
     $postLength = count($_POST);
 
-    if ($postLength != 2) {
+    if ($postLength != 6) {
         throw new Exception('Dados insuficientes');
     }
 
-    if (!isset($_GET['id']) || !isset($_POST['name']) || !isset($_POST['password'])) {
+    if (!isset($_GET['id']) || !isset($_POST['idCategoria']) || !isset($_POST['idEstado']) || !isset($_POST['name']) || !isset($_POST['numSerie']) || !isset($_POST['descricao']) || !isset($_POST['qrcode'])) {
         throw new Exception('Dados insuficientes. Preencha os dados corretamente');
     }
 
+    $idCategoria = trim($_POST['idCategoria']);
+    $idEstado = trim($_POST['idEstado']);
     $name = trim($_POST['name']);
-    $password = trim($_POST['password']);
+    $numSerie = trim($_POST['numSerie']);
+    $descricao = trim($_POST['descricao']);
+    $qrcode = trim($_POST['qrcode']);
 
-    if (strlen($name) == 0 || strlen($password) == 0) {
+    if (strlen($idCategoria) == 0 || strlen($idEstado) == 0 || strlen($name) == 0 || strlen($numSerie) == 0 || strlen($descricao) == 0 || strlen($qrcode) == 0) {
         throw new Exception('Dados insuficientes. Preencha os dados corretamente');
     }
 
     $id = $_GET['id'];
 
-    $sqlSelect = 'SELECT * FROM users WHERE id = :id';
+    $sqlSelect = 'SELECT * FROM equipamentos WHERE id = :id';
     $stmt = $pdo->prepare($sqlSelect);
     $stmt->bindParam(':id', $id, PDO::PARAM_INT);
     $stmt->execute();
@@ -53,11 +57,15 @@ try {
         throw new Exception('Erro ao criar registo.');
     }
 
-    $sqlUpdate = 'UPDATE users SET name = :name, password = :password WHERE id = :id';
+    $sqlUpdate = 'UPDATE equipamentos SET  id_categoria = :idCategoria, id_estado = :idEstado, name = :name, num_serie = :numSerie, descricao = :descricao, qrcode = :qrcode WHERE id = :id';
     $stmt = $pdo->prepare($sqlUpdate);
-    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
-    $stmt->bindParam(':password', $password, PDO::PARAM_STR);
     $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->bindParam(':idCategoria', $idCategoria, PDO::PARAM_INT);
+    $stmt->bindParam(':idEstado', $idEstado, PDO::PARAM_INT);
+    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+    $stmt->bindParam(':numSerie', $numSerie, PDO::PARAM_STR);
+    $stmt->bindParam(':descricao', $descricao, PDO::PARAM_STR);
+    $stmt->bindParam(':qrcode', $qrcode, PDO::PARAM_STR);
 
     if ($stmt->execute()) {
         $pdo->commit();
@@ -69,21 +77,24 @@ try {
         throw new Exception('Erro ao executar a query');
     }
 
-    $sqlSelectUpdated = 'SELECT * FROM users WHERE id = :id';
+    $sqlSelectUpdated = 'SELECT * FROM equipamentos WHERE id = :id';
     $stmt = $pdo->prepare($sqlSelectUpdated);
     $stmt->bindParam(':id', $id, PDO::PARAM_INT);
     $stmt->execute();
 
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    $equipamento = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if ($user) {
+    if ($equipamento) {
         $result = [
             'success' => true,
             'data' => [
-                'id' => $user['id'],
-                'name' => $user['name'],
-                'email' => $user['email'],
-                'password' => $user['password'],
+                'id' => $equipamento['id'],
+                'id_categoria' => $equipamento['id_categoria'],
+                'id_estado' => $equipamento['id_estado'],
+                'name' => $equipamento['name'],
+                'num_serie' => $equipamento['num_serie'],
+                'descricao' => $equipamento['descricao'],
+                'qrcode' => $equipamento['qrcode'],
             ]
         ];
         echo json_encode($result, JSON_PRETTY_PRINT);
